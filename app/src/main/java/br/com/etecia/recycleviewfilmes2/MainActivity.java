32ab81e5006f0ff387d2575001755cea7a05ab90
JohnatanSSantos <johnatansantos317@gmail.com>
2023-03-21 20:17:17 -0300

    criando apresenta filme layout

--- app/src/main/java/br/com/etecia/recycleviewfilmes2/MainActivity.java
+++ app/src/main/java/br/com/etecia/recycleviewfilmes2/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.recycleviewfilmes2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -11,10 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     List<Filmes> listaFilmes;
 
+
+    //Declarar o recycler view
+    RecyclerView idRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Apresentando o xml para o java
+        idRecyclerView = findViewById(R.id.idListaFilmes);
 
         //Criando a base de dados para carregamento da lista de filmes
         listaFilmes = new ArrayList<>();
@@ -24,8 +33,43 @@ public class MainActivity extends AppCompatActivity {
                         "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
                         "Drama, Suspense",
                         R.drawable.coringa));
+        listaFilmes.add(
+                new Filmes("Coringa",
+                        "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
+                        "Drama, Suspense",
+                        R.drawable.tomejerry));
+        listaFilmes.add(
+                new Filmes("Coringa",
+                        "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
+                        "Drama, Suspense",
+                        R.drawable.badboys));
+        listaFilmes.add(
+                new Filmes("Coringa",
+                        "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
+                        "Drama, Suspense",
+                        R.drawable.bob));
+        listaFilmes.add(
+                new Filmes("Coringa",
+                        "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
+                        "Drama, Suspense",
+                        R.drawable.area51));
+        listaFilmes.add(
+                new Filmes("Coringa",
+                        "'Coringa' é uma história independente e original. Arthur Fleck (Joaquin Phoenix), um homem ignorado pela sociedade, não é apenas um caso de estudo comportamental, mas uma lição de vida.",
+                        "Drama, Suspense",
+                        R.drawable.doom));
 
         //Criando a classe adaptadora e passando os parâmetros
-        MyAdapter adapter = new MyAdapter(getApplicationContext(),listaFilmes);
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), listaFilmes);
+
+        // Tipo de layout de a lista irá seguir
+        idRecyclerView.setLayoutManager(new
+                GridLayoutManager(
+                getApplicationContext(), 2));
+        //Fixador de tamanho da lista - deixar a lista mais rápida
+        idRecyclerView.setHasFixedSize(true);
+
+        //ligar o recycler view ao adaptador
+        idRecyclerView.setAdapter(adapter);
     }
 }
